fd5a707919a98fee98d926697e303175ddccabba
Taiza Moura <taizamoura2016@gmail.com>
2021-11-25 18:21:35 -0300

    soma

--- teste.py
+++ teste.py
@@ -1,3 +1,20 @@
-# Comentário de teste 
+#função que recebe dois parâmetros soma e devolve o resultado
+def soma(n1,n2):
+    resultado = n1+n2
+    return (resultado)
 
-print('Hello World')
+
+
+#main principal que rodará por toda a execução
+def main() -> None:
+    num1=int(input("digite o primeiro numero pra soma:"))
+    num2=int(input("digite o segundo numero pra soma:"))
+
+    #chama a função soma e ja printa o retorno
+    print(soma(num1,num2))
+
+
+
+#chama o main principal
+if __name__ == '__main__':
+    main()
