5cdf5fdd6cdea82f1f7f7aa9a8e4233479f415b1
Taiza Moura <taizamoura2016@gmail.com>
2021-11-25 22:59:22 -0300

    ultima alteração

--- teste.py
+++ teste.py
@@ -1,17 +1,17 @@
 #função que recebe dois parâmetros soma e devolve o resultado
-def soma(n1,n2):
-    resultado = n1+n2
-    return (resultado)
+def par(n1):
+    if n1%2==0:
+        print('eh par')
+    else:
+        printf('nao eh par')
 
 
 
 #main principal que rodará por toda a execução
 def main() -> None:
-    num1=int(input("digite o primeiro numero pra soma:"))
-    num2=int(input("digite o segundo numero pra soma:"))
-
-    #chama a função soma e ja printa o retorno
-    print(soma(num1,num2))
+    num1=int(input("digite o primeiro numero pra saber se eh par:"))
+    #chama a função par
+    par(num1)
 
 
 
